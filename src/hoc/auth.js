68628660f8fb0c0d500227c88b1f861204a52fba
Leonardo Souza <leogsouza@gmail.com>
2020-02-22 14:05:40 -0300

    Add Auth component

---
+++ src/hoc/auth.js
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { auth } from '../actions/actionUser'
+
+
+export default (ComponentClass, reload, adminRoute = null) => {
+  const AuthenticationCheck = (props) => {
+
+    let user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+
+    useEffect(async () => {
+      const response = await dispatch(auth())
+      if (!response.payload.isAuth) {
+        if (reload) {
+          props.history.push('/login')
+        }
+      } else {
+        if (adminRoute && !response.payload.isAdmin) {
+          props.history.push('/')
+        } else {
+          if (reload === false) {
+            props.history.push('/')
+          }
+        }
+      }
+    }, [])
+    return (
+      <ComponentClass {...props} user={user} />
+    )
+  }
+  return AuthenticationCheck
+}
